da14ee8502a0909d722c5686f34d64f9b0e6d2de
Amanda McNary <amanda.mcnary@stem.com>
2016-05-13 18:28:10 -0700

    changed classifier to return an object so we can use it later!

--- nltk_classify_bag_of_words.py
+++ nltk_classify_bag_of_words.py
@@ -1,6 +1,5 @@
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
-from nltk.corpus import movie_reviews
 import pandas as pd
 import urllib2
 import pdb
@@ -21,31 +20,39 @@ def load_csv_sentences(filename):
     return phrases
 
 
+class SuicideClassifier(object):
+    def __init__(self):
+        # neg_phrases = filter_negative_phrases(load_csv_sentences('thoughtsandfeelings.csv'))
+        # pos_phrases = filter_positive_phrases(load_csv_sentences('spiritualforums.csv'))
+        neg_file = open("neg_phrases.txt", "r")
+        pos_file = open("pos_phrases.txt", "r")
+        neg_phrases = neg_file.readlines()
+        pos_phrases = pos_file.readlines()
+
+        neg_phrases_tagged = []
+        pos_phrases_tagged = []
+        for phrase in neg_phrases:
+            neg_phrases_tagged.append((word_feats(phrase.split()), 'suicidal'))
+        for phrase in pos_phrases:
+            pos_phrases_tagged.append((word_feats(phrase.split()), 'alright'))
+
+        negcutoff = int(len(neg_phrases_tagged) * .8)
+        poscutoff = int(len(pos_phrases_tagged) * .8)
+
+        trainfeats = neg_phrases_tagged[:negcutoff] + pos_phrases_tagged[:poscutoff]
+        testfeats = neg_phrases_tagged[negcutoff:] + pos_phrases_tagged[poscutoff:]
+        print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
+
+        self.classifier = NaiveBayesClassifier.train(trainfeats)
+        print 'accuracy:', nltk.classify.util.accuracy(self.classifier, testfeats)
+        self.classifier.show_most_informative_features()
+
+    def test(self, phrase):
+        return self.classifier.classify(word_feats(phrase))
+
+
 def main():
-    # neg_phrases = filter_negative_phrases(load_csv_sentences('thoughtsandfeelings.csv'))
-    # pos_phrases = filter_positive_phrases(load_csv_sentences('spiritualforums.csv'))
-    neg_file = open("neg_phrases.txt", "r")
-    pos_file = open("pos_phrases.txt", "r")
-    neg_phrases = neg_file.readlines()
-    pos_phrases = pos_file.readlines()
-
-    neg_phrases_tagged = []
-    pos_phrases_tagged = []
-    for phrase in neg_phrases:
-        neg_phrases_tagged.append((word_feats(phrase.split()), 'suicidal'))
-    for phrase in pos_phrases:
-        pos_phrases_tagged.append((word_feats(phrase.split()), 'alright'))
-
-    negcutoff = len(neg_phrases_tagged)*3/4
-    poscutoff = len(pos_phrases_tagged)*3/4
-     
-    trainfeats = neg_phrases_tagged[:negcutoff] + pos_phrases_tagged[:poscutoff]
-    testfeats = neg_phrases_tagged[negcutoff:] + pos_phrases_tagged[poscutoff:]
-    print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
-     
-    classifier = NaiveBayesClassifier.train(trainfeats)
-    print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
-    classifier.show_most_informative_features()
+    return SuicideClassifier()
 
 
 if __name__ == '__main()__':
