5d457afb7eb9165f28f5412a1ec2a236a113fd6a
Meredith Marks <meredithgmarks@gmail.com>
2016-05-17 11:09:54 -0700

    unigrams interactive, maybe?

--- nltk_classify_unigrams.py
+++ nltk_classify_unigrams.py
@@ -1,16 +1,11 @@
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
-from nltk.corpus import movie_reviews
-from nltk.classify import NaiveBayesClassifier
-from nltk.corpus import subjectivity
 from nltk.sentiment import SentimentAnalyzer
 from nltk.sentiment.util import *
 import pandas as pd
 import urllib2
 import pdb
- 
-def word_feats(words):
-    return dict([(word, True) for word in words])
+
 
 def filter_negative_phrases(phrases):
     for phrase in phrases:
@@ -56,54 +51,69 @@ def load_csv_sentences(filename):
     phrases = [x.lower() for x in phrases if len(x) > 3 and len(x) < 200]
     return phrases
 
+class SuicideClassifier(object):
+    def __init__(self):
+        # neg_phrases = filter_negative_phrases(load_csv_sentences('thoughtsandfeelings.csv'))
+        # pos_phrases = filter_positive_phrases(load_csv_sentences('spiritualforums.csv'))
+        # file_pos = open("pos_phrases.txt", 'w')
+        # file_neg = open("neg_phrases.txt", 'w')
+
+        # for item in pos_phrases:
+        #     print>>file_pos, item
+        # for item in neg_phrases:
+        #     print>>file_neg, item
+
+        neg_file = open("neg_phrases.txt", "r")
+        pos_file = open("pos_phrases.txt", "r")
+        neg_phrases = neg_file.readlines()
+        pos_phrases = pos_file.readlines()
+
+        neg_docs = []
+        pos_docs = []
+        for phrase in neg_phrases:
+            neg_docs.append((phrase.split(), 'suicidal'))
+        for phrase in pos_phrases:
+            pos_docs.append((phrase.split(), 'alright'))
+
+        print len(neg_docs)
+        print len(pos_docs)
+        negcutoff = len(neg_docs)*3/4
+        poscutoff = len(pos_docs)*3/4
+
+        train_pos_docs = pos_docs[:poscutoff]
+        test_pos_docs = pos_docs[poscutoff:]
+        train_neg_docs = neg_docs[:negcutoff]
+        test_neg_docs = neg_docs[negcutoff:]
+        training_docs = train_pos_docs + train_neg_docs
+        testing_docs = test_pos_docs + test_neg_docs
+
+        self.sentim_analyzer = SentimentAnalyzer()
+        all_words = self.sentim_analyzer.all_words([doc for doc in training_docs])
+        unigram_feats = self.sentim_analyzer.unigram_word_feats(all_words, min_freq=2)
+        self.sentim_analyzer.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
+
+        # bigram_feats = sentim_analyzer.bigram_collocation_feats(all_words, min_freq=4)
+        # sentim_analyzer.add_feat_extractor(extract_bigram_feats, bigrams=bigram_feats)
+
+        training_set = self.sentim_analyzer.apply_features(training_docs)
+        test_set = self.sentim_analyzer.apply_features(testing_docs)
+        trainer = NaiveBayesClassifier.train
+        self.classifier = self.sentim_analyzer.train(trainer, training_set)
+        for key,value in sorted(self.sentim_analyzer.evaluate(test_set).items()):
+            print('{0}: {1}'.format(key, value))
+
+    def test(self, phrase):
+        all_words = self.sentim_analyzer.all_words(phrase)
+        unigram_feats = self.sentim_analyzer.unigram_word_feats(all_words)
+        return self.classifier.classify(unigram_feats)
+
 def main():
-    # neg_phrases = filter_negative_phrases(load_csv_sentences('thoughtsandfeelings.csv'))
-    # pos_phrases = filter_positive_phrases(load_csv_sentences('spiritualforums.csv'))
-    # file_pos = open("pos_phrases.txt", 'w')
-    # file_neg = open("neg_phrases.txt", 'w')
-
-    # for item in pos_phrases:
-    #     print>>file_pos, item
-    # for item in neg_phrases:
-    #     print>>file_neg, item
-
-    neg_file = open("neg_phrases.txt", "r")
-    pos_file = open("pos_phrases.txt", "r")
-    neg_phrases = neg_file.readlines()
-    pos_phrases = pos_file.readlines()
-
-    neg_docs = []
-    pos_docs = []
-    for phrase in neg_phrases:
-        neg_docs.append((phrase.split(), 'suicidal'))
-    for phrase in pos_phrases:
-        pos_docs.append((phrase.split(), 'alright'))
-
-    print len(neg_docs)
-    print len(pos_docs)
-    negcutoff = len(neg_docs)*3/4
-    poscutoff = len(pos_docs)*3/4
-
-    train_pos_docs = pos_docs[:poscutoff]
-    test_pos_docs = pos_docs[poscutoff:]
-    train_neg_docs = neg_docs[:negcutoff]
-    test_neg_docs = neg_docs[negcutoff:]
-    training_docs = train_pos_docs + train_neg_docs
-    testing_docs = test_pos_docs + test_neg_docs
-    sentim_analyzer = SentimentAnalyzer()
-    all_words = sentim_analyzer.all_words([doc for doc in training_docs])
-    unigram_feats = sentim_analyzer.unigram_word_feats(all_words, min_freq=4)
-    sentim_analyzer.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
-
-    # bigram_feats = sentim_analyzer.bigram_collocation_feats(all_words, min_freq=4)
-    # sentim_analyzer.add_feat_extractor(extract_bigram_feats, bigrams=bigram_feats)
-
-    training_set = sentim_analyzer.apply_features(training_docs)
-    test_set = sentim_analyzer.apply_features(testing_docs)
-    trainer = NaiveBayesClassifier.train
-    classifier = sentim_analyzer.train(trainer, training_set)
-    for key,value in sorted(sentim_analyzer.evaluate(test_set).items()):
-        print('{0}: {1}'.format(key, value))
+    classifier = SuicideClassifier()
+    test_string = ''
+    print 'Welcome!'
+    while str(test_string) not in ('q', 'quit', 'exit'):
+        test_string = raw_input('Enter phrase to test: ')
+        print(classifier.test(str(test_string)))
 
 if __name__ == '__main()__':
     main()
